eb2b7cff8302fba636a3866a2f4b38093c6475b4
Amy Schlesener <amyschlesener@gmail.com>
2016-06-30 21:28:14 -0700

    Add tag model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ActiveRecord::Base
+	has_many :taggings
+	has_many :tags, through: :taggings
 	validates :title, presence: true, length: { minimum: 2,  maximum: 50 }
 	validates :description, presence: true, length: { minimum: 10,  maximum: 100 }
 	validates :salary, :inclusion => 0..300000, allow_nil: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701031237) do
+ActiveRecord::Schema.define(version: 20160701042319) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "title"
@@ -19,6 +19,22 @@ ActiveRecord::Schema.define(version: 20160701031237) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "salary"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "job_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["job_id"], name: "index_taggings_on_job_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
